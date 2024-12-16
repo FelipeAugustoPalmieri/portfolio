2e8eaa9e5f8ca2256f5b093b9cbc95cb53f45b4c
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2024-12-16 11:14:45 -0300

    feat: structure initial nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import Nav from "@/Components/Nav";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="overflow-x-hidden">
+      <div>
+        <Nav />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
