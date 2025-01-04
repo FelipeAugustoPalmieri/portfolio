91c21442c403698a855641865b6aa020ff400f2d
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2025-01-04 13:06:55 -0300

    fix: corrections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
+    <nav className="mb-20 flex items-center justify-between py-6 overflow-x-hidden text-neutral-300">
       <div className="flex flex-shrink-0 items-center">Gif lottie</div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
