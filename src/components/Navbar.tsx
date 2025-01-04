d57f6fa763dc2280046ef8926ac1c4b69f0a0ed7
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2025-01-04 12:52:09 -0300

    feat: base initial Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">Gif lottie</div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
