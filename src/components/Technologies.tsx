a10dd54186a42cbc422c1891c8c1db52efed384e
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2025-01-06 23:37:16 -0300

    implement animation with framer-motion

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,44 +1,103 @@
+"use client";
+
 import React from "react";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { FaLaravel, FaNodeJs } from "react-icons/fa";
 import { IoLogoDocker } from "react-icons/io5";
-import { LiaDocker } from "react-icons/lia";
 import { RiReactjsLine } from "react-icons/ri";
 import { SiFirebase, SiNestjs } from "react-icons/si";
 import { TbBrandNextjs, TbBrandTypescript } from "react-icons/tb";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration: number) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [0, -10, 0], // Vai para cima e volta para a posição original
+    transition: {
+      duration: duration, // Tempo customizado
+      ease: "easeInOut", // Transição suave
+      repeat: Infinity, // Repetição infinita
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
       <h2 className="my-20 text-center text-4xl">Technologies</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaLaravel className="text-7xl text-red-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaNodeJs className="text-7xl text-green-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <TbBrandTypescript className="text-7xl text-customBlue" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiNestjs className="text-7xl text-red-600" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <TbBrandNextjs className="text-7xl" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <IoLogoDocker className="text-7xl text-blue-600" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiFirebase className="text-7xl text-orange-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <BiLogoPostgresql className="text-7xl text-sky-700" />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
