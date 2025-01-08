f52e7cdeb85fb1adfcf47c17e874e644ec453cb9
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2025-01-08 00:47:34 -0300

    I created component Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,35 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+import ProjectCard from "./ProjectCard";
+import { PROJECTS } from "@/constants";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h2>
+      <div className="grid md:grid-cols-3 gap-8 md:gap:12">
+        {PROJECTS.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            image={project.image}
+            githubLink={project.githubLink}
+            previewLink={project.previewLink}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
