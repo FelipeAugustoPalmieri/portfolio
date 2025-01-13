a612afea256b1eb655f235084f8c40859c436dde
FelipeAugustoPalmieri <felipepalmieridev@gmail.com>
2025-01-13 00:50:46 -0300

    feat: create filters tags

---
+++ src/components/ProjectsTag.tsx
@@ -0,0 +1,28 @@
+import { button } from "framer-motion/client";
+import React from "react";
+
+interface ProjectTagProps {
+  name: string;
+  onClick: (tag: string) => void;
+  isSelected: boolean;
+}
+
+const ProjectsTag: React.FC<ProjectTagProps> = ({
+  name,
+  onClick,
+  isSelected,
+}) => {
+  const buttonStyles = isSelected
+    ? "text-white border-purple-500"
+    : "text-[#ADB7BE] border-slate-600";
+  return (
+    <button
+      className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+      onClick={() => onClick(name)}
+    >
+      {name}
+    </button>
+  );
+};
+
+export default ProjectsTag;
